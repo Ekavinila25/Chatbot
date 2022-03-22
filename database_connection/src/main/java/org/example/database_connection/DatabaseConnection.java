893d7982e4d04ff6e819f47dd887d9b2eeaa0cd3
Ekavinila25 <98815067+Ekavinila25@users.noreply.github.com>
2022-03-22 19:45:20 +0530

    Update DatabaseConnection.java

--- database_connection/src/main/java/org/example/database_connection/DatabaseConnection.java
+++ database_connection/src/main/java/org/example/database_connection/DatabaseConnection.java
@@ -1,38 +1,5 @@
 package org.example.database_connection;
 
-//import org.example.database_connection.exception.ConnectionException;
-//import sun.jvm.hotspot.debugger.DataSource;
-//import com.mysql.cj.jdbc.MysqlDataSource;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//import java.util.ResourceBundle;
-//
-///**
-// * Gets connection.
-// *
-// * @author KavinilaE
-// */
-//public class DatabaseConnection {
-//
-//    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/kavinila";
-//    private static final String DATABASE_NAME = "root";
-//    private static final String DATABASE_PASSWORD = "Kavi@967761";
-//    private static Connection connection;
-//
-//    public static final Connection getConnection() {
-//
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            connection = DriverManager.getConnection(JDBC_URL, DATABASE_NAME, DATABASE_PASSWORD);
-//        } catch (Exception exception) {
-//            throw new ConnectionException("Connection Failed");
-//        }
-//        return connection;
-//    }
-//
-//}
-
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -51,15 +18,7 @@ public class DatabaseConnection {
     public static Connection getConnection() {
 
         try {
-//            InputStream fileInputStream = new FileInputStream("C:/karaf/etc/system.properties");
-//            Properties properties = new Properties();
-//            properties.load(fileInputStream);
-//            String url = properties.getProperty("MySQL.path");
-//            String username = properties.getProperty("MySQL.user");
-//            String password = properties.getProperty("MySQL.pass");
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            final Connection connection = DriverManager.getConnection(url, username, password);
-//            return connection;
+         return connection;
             Class.forName("com.mysql.cj.jdbc.Driver");
             final Connection connection = DriverManager.getConnection(property.get("MySQL.path"),
                     property.get("MySQL.user"), property.get("MySQL.pass"));
